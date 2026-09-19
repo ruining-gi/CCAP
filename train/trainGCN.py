ab58116b555ccdabc73953fc9e39b62d76801608
ruining-gi <3230515237@qq.com>
2026-09-20 00:36:01 +0800

    Update trainGCN.py

--- train/trainGCN.py
+++ train/trainGCN.py
@@ -26,16 +26,16 @@ x_transforms = transforms.Compose([
 y_transforms = transforms.ToTensor()
 
 # 路径配置
-train_image_dir = "/mnt/diskb5/ruining_private/CHENXI/adjust/reconstructiondata/train/image/"
-train_mask_dir  = "/mnt/diskb5/ruining_private/CHENXI/adjust/reconstructiondata/train/mask/"
-val_image_dir   = "/mnt/diskb5/ruining_private/CHENXI/adjust/reconstructiondata/val/image/"
-val_mask_dir    = "/mnt/diskb5/ruining_private/CHENXI/adjust/reconstructiondata/val/mask/"
-test_image_dir  = "/mnt/diskb5/ruining_private/CHENXI/adjust/testtrain/image/"
-test_mask_dir   = "/mnt/diskb5/ruining_private/CHENXI/adjust/testtrain/mask/" 
+train_image_dir = ""
+train_mask_dir  = ""
+val_image_dir   = ""
+val_mask_dir    = ""
+test_image_dir  = ""
+test_mask_dir   = "" 
 
-save_vis_dir = "/mnt/diskb5/ruining_private/CHENXI/adjust/GCN/result_testtrain/gcn_visualizations/"
+save_vis_dir = ""
 # 新增：单独存储分割结果（预测掩码）的文件夹
-save_seg_dir = "/mnt/diskb5/ruining_private/CHENXI/adjust/GCN/result_testtrain/gcn_segmentations/"
+save_seg_dir = ""
 
 os.makedirs(save_vis_dir, exist_ok=True)
 os.makedirs(save_seg_dir, exist_ok=True)  # 创建分割结果文件夹
